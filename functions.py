e8ed0ff33305c940bb227e553a210f02a21e2b89
Shawn Ray <sray@gradcenter.cuny.edu>
2024-08-14 15:41:41 -0400

    add new line :)

--- functions.py
+++ functions.py
@@ -2,22 +2,23 @@
 # Replace the "raise NotImplementedError" line with the correct code!
    
 def print_the_instructors_name():
-    raise NotImplementedError
+    print("DoodleBob")
 
 def print_the_number_of_this_room():
-    raise NotImplementedError
+    print(5382)
 
 def split_this_string_at_each_space(input_string):
-    raise NotImplementedError
+    print(input_string.split())
 
 def take_the_average_of_these_numbers(a, b):
-    raise NotImplementedError
+    print((a+b)/2)
 
 def print_the_time_now_using_astropy():
-    raise NotImplementedError
+    print(astropy.time)
 
 def return_the_minimum_of_two_numbers(a, b):
-    raise NotImplementedError
+    print(numpy.minimum(a,b))
 
 def return_the_std_of_two_numbers(a, b):
-    raise NotImplementedError
+    print(numpy.std(a,b))
+
